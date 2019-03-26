d51dbb2031620637359312dab9a18ecb78c311b6
Colin Curtis <ckcurtis@renci.org>
2019-03-26 15:06:06 -0400

    tasks for each endpoint added

--- uberonto_api_locust.py
+++ uberonto_api_locust.py
@@ -1,14 +1,25 @@
 from locust import HttpLocust, TaskSet, task
 
 class UserBehavior(TaskSet):
-    #1
+    
     @task(1)
     def get_descendants(self):
         self.client.get("/descendants/CHEBI:23367")
-    
+
+    @task(1)
+    def get_children(self):
+        self.client.get("/children/GO:0005576")
+
+    @task(1)
+    def get_id_list(self):
+        self.client.get("/id_list/MONDO")
+
+    @task(1)
+    def get_label(self):
+        self.client.get("/label/MONDO:0004979")
 
 
 class WebsiteUser(HttpLocust):
     task_set = UserBehavior
-    min_wait = 0
+    min_wait = 100
     max_wait = 1000
